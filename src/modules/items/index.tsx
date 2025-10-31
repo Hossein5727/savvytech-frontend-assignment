887e26c844cec224a33d22f833934ba33e63a3ad
Hossein5727 <hosseinghiasi5727@gmail.com>
2025-10-31 20:27:21 +0330

    Enhance Items component layout with Add New Item button
    - Introduced a new button with a PlusIcon for adding items, improving user interaction.
    - Updated the layout to include the button alongside the title for better visual structure.

--- src/modules/items/index.tsx
+++ src/modules/items/index.tsx
@@ -1,9 +1,17 @@
 import ItemList from "./components/Item-list";
+import { Button } from "@/share-components/atmos/button/default";
+import { PlusIcon } from "lucide-react";
 
 function Items() {
   return (
     <div className="space-y-8 h-screen">
-      <h1 className="text-3xl">Savvytech Frontend Assignment</h1>
+      <div className="flex justify-between items-center">
+        <h1 className="text-3xl">Savvytech Frontend Assignment</h1>
+        <Button>
+          <PlusIcon className="w-4 h-4 mr-2" />
+          Add New Item
+        </Button>
+      </div>
       <ItemList />
     </div>
   );
