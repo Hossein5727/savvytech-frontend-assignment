fe376de14578dda9dfcb69351b2ebee46ecbfae1
Hossein5727 <hosseinghiasi5727@gmail.com>
2025-11-01 13:21:27 +0330

    Enhance CreateItemModal with item addition functionality and improved input handling
    - Integrated zustand store to manage item creation within CreateItemModal.
    - Added form validation to ensure all fields are filled before submission.
    - Refactored input components for better readability and maintainability.

--- src/modules/items/components/create-item-modal.tsx
+++ src/modules/items/components/create-item-modal.tsx
@@ -1,9 +1,18 @@
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter, DialogClose } from "@/share-components/molecules/dialog/default";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+  DialogFooter,
+  DialogClose,
+} from "@/share-components/molecules/dialog/default";
 import { Label } from "@/share-components/atoms/label/default";
 import { Input } from "@/share-components/atoms/input/default";
 import { Button } from "@/share-components/atoms/button/default";
 import { useState } from "react";
 import { toast } from "sonner";
+import { useItemsStore } from "../store/itemsStore";
 
 function CreateItemModal({
   open,
@@ -12,17 +21,23 @@ function CreateItemModal({
   open: boolean;
   setOpen: (open: boolean) => void;
 }) {
+  const { addItem } = useItemsStore();
   const [form, setForm] = useState({
     title: "",
     subtitle: "",
   });
 
   const handleSubmit = () => {
-
-    if(form.title === "" || form.subtitle === "") {
+    if (form.title === "" || form.subtitle === "") {
       toast.error("Please fill out all fields");
       return;
     }
+    addItem({
+      id: crypto.randomUUID(),
+      title: form.title,
+      subtitle: form.subtitle,
+      date_created: new Date().toISOString().split("T")[0],
+    });
     toast.success("Item created successfully");
     setOpen(false);
     setForm({
@@ -45,13 +60,20 @@ function CreateItemModal({
             <div className="grid gap-4">
               <div className="grid gap-3">
                 <Label htmlFor="title">Title</Label>
-                <Input value={form.title} onChange={(e) => setForm({ ...form, title: e.target.value })} id="title" name="title" />
+                <Input
+                  value={form.title}
+                  onChange={(e) => setForm({ ...form, title: e.target.value })}
+                  id="title"
+                  name="title"
+                />
               </div>
               <div className="grid gap-3">
                 <Label htmlFor="subtitle">Subtitle</Label>
                 <Input
                   value={form.subtitle}
-                  onChange={(e) => setForm({ ...form, subtitle: e.target.value })}
+                  onChange={(e) =>
+                    setForm({ ...form, subtitle: e.target.value })
+                  }
                   id="subtitle"
                   name="subtitle"
                 />
@@ -59,9 +81,13 @@ function CreateItemModal({
             </div>
             <DialogFooter>
               <DialogClose asChild>
-                <Button variant="outline" onClick={() => setOpen(false)}>Cancel</Button>
+                <Button variant="outline" onClick={() => setOpen(false)}>
+                  Cancel
+                </Button>
               </DialogClose>
-              <Button type="submit" onClick={handleSubmit}>Create Item</Button>
+              <Button type="submit" onClick={handleSubmit}>
+                Create Item
+              </Button>
             </DialogFooter>
           </DialogContent>
         </div>
